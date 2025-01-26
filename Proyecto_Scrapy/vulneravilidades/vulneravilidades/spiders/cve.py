66c8a358c1bdcf221aae33d7c28e49beafc58528
argelio <argelio17@hotmail.com>
2025-01-26 16:17:04 -0500

    se trabaja con el archivo de Curso_Scritp_python_y_SQL.ipynb

---
+++ Proyecto_Scrapy/vulneravilidades/vulneravilidades/spiders/cve.py
@@ -0,0 +1,21 @@
+import scrapy
+
+
+class CveSpider(scrapy.Spider):
+    name = "cve"
+    allowed_domains = ["https://cve.mitre.org"]
+    start_urls = ["https://cve.mitre.org/data/refs/refmap/source-EXPLOIT-DB.html"]
+
+    def parse(self, response):
+        for child in response.xpath('//table'):
+            if len(child.xpath('tr'))>100:
+                table=child
+                break
+            
+        for row in table.xpath('//tr'):
+            try:
+                print(row.xpath('td//text()'[0].extract))
+                
+            except IndexError:
+                pass
+            
