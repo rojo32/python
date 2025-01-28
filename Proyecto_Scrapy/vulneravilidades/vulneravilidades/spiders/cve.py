a36cdfe2b5a7af549d512c099881496138681d8f
rojo32 <argelio17@hotmail.com>
2025-01-27 22:16:09 -0500

    se trabaja en el archivo cve.py

--- Proyecto_Scrapy/vulneravilidades/vulneravilidades/spiders/cve.py
+++ Proyecto_Scrapy/vulneravilidades/vulneravilidades/spiders/cve.py
@@ -1,5 +1,7 @@
 import scrapy
-
+import csv
+import json
+import sqlite3
 
 class CveSpider(scrapy.Spider):
     name = "cve"
@@ -7,15 +9,42 @@ class CveSpider(scrapy.Spider):
     start_urls = ["https://cve.mitre.org/data/refs/refmap/source-EXPLOIT-DB.html"]
 
     def parse(self, response):
+        conexion=sqlite3.connect('vuln.db')
+        tabla='CREATE TABLE vulns(exploit TEXT, cve TEXT)'
+        cursor= conexion.cursor()
+        cursor.execute(tabla)
+        conexion.commit()
+        
         for child in response.xpath('//table'):
             if len(child.xpath('tr'))>100:
                 table=child
                 break
+        
+        count=0
+        #data={}
+        #csv_file=open('vulnerabilidades.csv','w')
+        #json_file=open('vulnerabilidades.json','w')
+        #write=csv.writer(csv_file)
+        #dividmos el archivo en dos columnas
+        #write.writerow(['exploit id','cve id'])
+              
+            
             
         for row in table.xpath('//tr'):
             try:
-                print(row.xpath('td//text()'[0].extract))
+                exploit_id =row.xpath('td//text()')[0].extract()
+                cve_id=row.xpath('td//text()')[2].extract()
+                #write.writerow([exploit_id,cve_id])
+                #data[exploit_id]=cve_id
+                cursor.execute('INSERT INTO vulns (exploit, cve) VALUES(?,?)',(exploit_id,cve_id))
+                conexion.commit()
+                
+                count+=1                
                 
             except IndexError:
                 pass
+        
+        #csv_file.close()
+        #json.dump(data,json_file)
+        #json_file.close()
             
